23f19a20e527428e652338f5c3596141355ead71
Kejdi <kejdimuci.dev@gmail.com>
2026-01-21 12:48:57 +0100

    testing kejdi 1

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,27 +6,28 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import view.LoginView;
 
+//s
+public class Main extends Application {
 
-public class Main extends Application{
-	private static Scene scene;
-	private static Stage stage;
-	
-	@Override
-	public void start(Stage primaryStage) {
-		stage = primaryStage;
-		scene = new Scene(new LoginView(), 1150, 600);
-		displayOnScene(new LoginView());
-		scene.getStylesheets().add(getClass().getResource("/view/ui/styles.css").toExternalForm());
-		stage.setScene(scene);
-		stage.setTitle("Electronics Store Software");
-		stage.show();
-	}
-	
-	public static void displayOnScene(Pane pane) {
-		scene.setRoot(pane);
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    private static Scene scene;
+    private static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) {
+        stage = primaryStage;
+        scene = new Scene(new LoginView(), 1150, 600);
+        displayOnScene(new LoginView());
+        scene.getStylesheets().add(getClass().getResource("/view/ui/styles.css").toExternalForm());
+        stage.setScene(scene);
+        stage.setTitle("Electronics Store Software");
+        stage.show();
+    }
+
+    public static void displayOnScene(Pane pane) {
+        scene.setRoot(pane);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
